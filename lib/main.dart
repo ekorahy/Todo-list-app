26d78ccb0f88da0506d3e88b1b2d14fd479c8feb
ekorahy <ekorahy@gmail.com>
2023-01-02 02:22:25 +0700

    change name and icon app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_list_app/Service/Auth_Service.dart';
-import 'package:todo_list_app/pages/AddTodo.dart';
 import 'package:todo_list_app/pages/HomePage.dart';
 import 'package:todo_list_app/pages/SignInPage.dart';
-import 'package:todo_list_app/pages/SignUpPage.dart';
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
